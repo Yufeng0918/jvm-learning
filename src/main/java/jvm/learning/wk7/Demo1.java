0eaba34dfe128dbadef795def10b0aa9f3f94d30
daiyu <jiangyufeng.0918@gmail.com>
2020-07-20 08:18:07 +0800

    jvm第七周

---
+++ src/main/java/jvm/learning/wk7/Demo1.java
@@ -0,0 +1,13 @@
+package jvm.learning.wk7;
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+        byte[] array1 = new byte[1024 * 1024];
+        array1 = new byte[1024 * 1024];
+        array1 = new byte[1024 * 1024];
+        array1 = null;
+
+        byte[] array2 = new byte[2 * 1024 * 1024];
+    }
+}
