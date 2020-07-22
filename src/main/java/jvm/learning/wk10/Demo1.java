6dfd8d31a163da4102fe57fae56d643623ba37f7
daiyu <jiangyufeng.0918@gmail.com>
2020-07-22 08:57:32 +0800

    jvm第十周

---
+++ src/main/java/jvm/learning/wk10/Demo1.java
@@ -0,0 +1,19 @@
+package jvm.learning.wk10;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo1 {
+
+    public static void main(String[] args) throws Exception {
+        List<Data> datas = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            datas.add(new Data());
+        }
+        Thread.sleep(1 * 60 *60 * 10000);
+    }
+
+
+    static class Data {
+    }
+}
